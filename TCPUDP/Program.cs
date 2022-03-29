8e2f8756110152f45dc8ca76caf673c18b9e82d1
Morozov <libertariansessions@gmail.com>
2022-03-29 20:36:09 +0500

    Add project files.

---
+++ TCPUDP/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TCPUDP
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var ips = new List<IPAddress>();
+            var startIp = "0.8.8.8";
+            var endIp = "8.8.8.8";
+            var startParsedIp = IPAddress.Parse(startIp);
+            var endParsedIp = IPAddress.Parse(endIp);
+            var start = startParsedIp.GetAddressBytes();
+            var end = endParsedIp.GetAddressBytes();
+            var startLong = FromBytes(start);
+            var endLong = FromBytes(end);
+            for (long i = startLong; i <= endLong; i+=1)
+            {
+                var vs = ToBytes(i);
+                var j = FromBytes(new byte[] {vs[3], vs[2], vs[1], vs[0]});
+                ips.Add(new IPAddress(j));
+            }
+
+            //ips = new List<IPAddress>() { IPAddress.Parse("87.240.139.193"), IPAddress.Parse("87.240.139.194"), IPAddress.Parse("87.240.139.195") };
+
+            var tcpPorts = Enumerable.Range(0, 800).ToArray();
+            var udpPorts = Enumerable.Range(0, 800).ToArray();
+            foreach (var ip in ips)
+            {
+                await Scanner.ScanTCP(ip, tcpPorts);
+                await Scanner.ScanUDP(ip, udpPorts);
+            }
+        }
+        
+        public static long FromBytes(byte[] vs)
+        {
+            return (long)vs[3] * 256 * 256 * 256 + (long)vs[2] * 256 * 256 + (long)vs[1] * 256 + (long)vs[0];
+        }
+
+        public static byte[] ToBytes(long vs)
+        {
+            return new byte[] { (byte)(vs % 256), (byte)(vs / 256 % 256), (byte)(vs / (256 * 256) % 256), (byte)(vs / (256 * 256 * 256) % 256)};
+        }
+    }
+}
---
+++ TCPUDP/Scanner.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCPUDP
+{
+    internal class Scanner
+    {
+        public static async Task ScanTCP(IPAddress ip, int[] ports)
+        {
+            List<Task> tasks = new();
+            foreach (var port in ports)
+            {
+                var dest = new IPEndPoint(ip, port);
+                tasks.Add(ScanPortTCP(dest, 100));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        public static async Task ScanPortTCP(IPEndPoint ipPort, int timeout)
+        {
+            using var client = new TcpClient();
+            try
+            {
+                var ct = new CancellationTokenSource(timeout);
+                var res = client.ConnectAsync(ipPort, ct.Token);
+                await res;
+                
+                if (res.IsCompletedSuccessfully)
+                {
+                    Console.WriteLine($"{ipPort} TCP open");
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                client.Close();
+                client.Dispose();
+            }
+        }
+
+        public static async Task ScanPortUDP(IPEndPoint ipPort, int timeout)
+        {
+            try
+            {
+                using var client = new UdpClient(new IPEndPoint(IPAddress.Parse("192.168.0.102"), ipPort.Port));
+                var ct = new CancellationTokenSource(timeout);
+                var send = client.SendAsync(new byte[] { 123 }, ipPort, ct.Token);
+                await send;
+                var recieve = client.ReceiveAsync(ct.Token);
+                await recieve;
+
+                if (recieve.IsCompletedSuccessfully)
+                {
+                    Console.WriteLine($"{ipPort} UDP closed");
+                }
+            }
+            catch
+            {
+                
+            }
+        }
+
+        public static async Task ScanUDP(IPAddress ip, int[] ports)
+        {
+            List<Task> tasks = new();
+            foreach (var port in ports)
+            {
+                var dest = new IPEndPoint(ip, port);
+                tasks.Add(ScanPortUDP(dest, 300));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
